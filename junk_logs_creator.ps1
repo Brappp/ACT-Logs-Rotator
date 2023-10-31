4a8484bbb0dd91211be378f4aa9c08af1a8b74c3
Wah <74884113+Brappp@users.noreply.github.com>
2023-10-30 23:24:38 -0400

    Update junk_logs_creator.ps1

--- junk_logs_creator.ps1
+++ junk_logs_creator.ps1
@@ -1,21 +1,17 @@
-﻿# Define the directory where you want to generate the fake log files
-$directoryPath = "$env:APPDATA\Advanced Combat Tracker\FFXIVLogs"
+﻿$directoryPath = "$env:APPDATA\Advanced Combat Tracker\FFXIVLogs"
 
-# Create the directory if it doesn't exist
 if (-Not (Test-Path $directoryPath)) {
     New-Item -Path $directoryPath -ItemType Directory
 }
 
-# Generate 10 fake log files
+
 for ($i = 0; $i -lt 10000; $i++) {
-    # Create a file name with the current date and index
+
     $fileName = "log_" + (Get-Date).ToString("yyyyMMdd") + "_$i.log"
     $filePath = Join-Path -Path $directoryPath -ChildPath $fileName
 
-    # Create the log file
     New-Item -Path $filePath -ItemType File
 
-    # Write some fake log content
     Add-Content -Path $filePath -Value "This is a fake log entry."
 
     # Modify the creation time to simulate older logs
